d8db0521f1e6d8255cc274b4f864c4bfae56eede
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-02-16 15:08:53 -0500

    Update PlayerMovement.cs
     Added Comments and tooltips to help in learning Unity.

--- HackathonGame/Assets/Scripts/PlayerMovement.cs
+++ HackathonGame/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Tooltip("This is the player speed. This is a float.")]
     public float playerSpeed;
 
     // Start is called before the first frame update
@@ -13,29 +13,34 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.W))
+        #region Player Movement
+
+        if (Input.GetKey(KeyCode.W)) // Moving player up 
         {
-            transform.position += new Vector3(0, playerSpeed * Time.deltaTime, 0);
+            transform.position += new Vector3(0, playerSpeed * Time.deltaTime, 0); // Might need to .nomarlize to keep diagonals at same speed as cardinals.
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)) // Moving player down
         {
             transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)) // Moving player right
         {
             transform.position += new Vector3(playerSpeed * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) // Moving Player left
         {
             transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0, 0);
         }
+
+        #endregion
 
     }
 }

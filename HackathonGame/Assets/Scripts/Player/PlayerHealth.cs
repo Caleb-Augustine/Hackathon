494d4e87452d6245ef1f11145e1ffb0f7fcacb41
cattomic135 <132307640+cattomic135@users.noreply.github.com>
2024-02-16 16:00:33 -0500

    Player Health
    Added player health and stuff

---
+++ HackathonGame/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Tooltip("This is the total player health. This is an int.")]
+    public int playerHealth; 
+    [Tooltip("This is the player base health. This is an int.")]
+    public int playerBaseHealth;
+    [Tooltip("This is the current player health. This is an int.")]
+    public int currHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = playerHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

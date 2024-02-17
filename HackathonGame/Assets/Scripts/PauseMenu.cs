81afaa7ed5e2009b4bc9804c0c8bb724bc4a6943
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-02-17 16:41:56 -0500

    Pause Menu
    Pause Menu Script added

---
+++ HackathonGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+
+
+
+    void Start()
+    {
+        isPaused = false;    
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+
+    }
+
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("We quitting this hoe fr fr.");
+    }
+
+}

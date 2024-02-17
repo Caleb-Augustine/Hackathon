5c9353d77ffc7ec43e90b442af5d5813f2fe0659
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-02-17 16:03:32 -0500

    Title Screen
    We now have a working title screen.

---
+++ HackathonGame/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpitionMenu()
+    {
+        //Do Pause Menu Here;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("We quittting this bitch fr fr.");
+    }
+
+
+
+}
--- HackathonGame/Assets/Scripts/Player/PlayerMovement.cs
+++ HackathonGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [Tooltip("This is the player speed. This is a float.")]

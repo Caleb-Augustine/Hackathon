a5ebdd286ec5dc7b860c552c6885c634095d7591
Caleb-Augustine <Calebaugustine117@gmail.com>
2024-02-16 21:48:16 -0500

    Enemy Scriptable
    Enemy Scriptable Asset

---
+++ HackathonGame/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+
+    public EnemyScriptable enemyType;
+
+    int currentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = enemyType.enemyHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void HealthCheck()
+    {
+        if (currentHealth <= 0)
+        {
+            //Spawn Coin function here.
+            GetComponent<SpriteRenderer>().enabled = false;
+            Destroy(gameObject, 1f);
+        }
+    }
+
+
+}
---
+++ HackathonGame/Assets/Scripts/Enemy/EnemyScriptable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Enemy")]
+public class EnemyScriptable : ScriptableObject
+{
+    [Tooltip("How much health an enemy has. This is an int.")]
+    public int enemyHealth;
+    [Tooltip("How far an enemy has to be before the attack starts. This is an int.")]
+    public int enemyRange;
+    [Tooltip("The amount of damage an enemy will do to the player. This is an int.")]
+    public int enemyDamage;
+    [Tooltip("The amount of coins dropped by the enemy on death. This is an int.")]
+    public int coinDropAmount = 1;
+    [Tooltip("The prefab for the enemy type.")]
+    public GameObject enemyPrefab;
+
+
+
+}
